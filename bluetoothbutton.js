dead754645ebb1c32fcd1075dfaa024187cf5a39
明和自動車 <arrowmeiwaracing@users.noreply.github.com>
2017-11-19 15:50:00 +0900

    Add files via upload

--- bluetoothbutton.js
+++ bluetoothbutton.js
@@ -20,39 +20,38 @@ module.exports = function(RED) {
             //msg.node-input-name;
              node.log(msg.payload);
 
-        node.child = spawn(pycmd + ".py", [msg.payload]);
-        node.status({fill:"green",shape:"dot",text:"common.status.ok"});
-        node.log("ready");
+            node.child = spawn(pycmd + ".py", [msg.payload]);
+            node.status({fill:"green",shape:"dot",text:"common.status.ok"});
+            node.log("ready");
 
-        node.child.stdout.on('data', function (data) {
-            //node.log(data);
-            var b = data.toString().trim().split(",");
-            var act = "up";
-            node.send({ topic:"pi/key", payload:{code: Number(b[0]), mode: Number(b[1]), tvsec: Number(b[2]), tvusec: Number(b[3])} });
-        });
+            node.child.stdout.on('data', function (data) {
+                //node.log(data);
+                var b = data.toString().trim().split(",");
+                var act = "up";
+                node.send({ topic:"pi/key", payload:{code: Number(b[0]), mode: Number(b[1]), tvsec: Number(b[2]), tvusec: Number(b[3])} });
+            });
 
-        node.child.stderr.on('data', function (data) {
-            if (RED.settings.verbose) { node.log("err: "+data+" :"); }
-        });
+            node.child.stderr.on('data', function (data) {
+                if (RED.settings.verbose) { node.log("err: "+data+" :"); }
+            });
 
-        node.child.on('close', function (code) {
-            node.running = false;
-            node.child = null;
-            if (RED.settings.verbose) { node.log(RED._("bluetoothbutton.status.closed")); }
-            if (node.done) {
-                node.status({fill:"grey",shape:"ring",text:"bluetoothbutton.status.closed"});
-                node.done();
-            }
-            else { node.status({fill:"red",shape:"ring",text:"bluetoothbutton.status.stopped"}); }
-        });
+            node.child.on('close', function (code) {
+                node.running = false;
+                node.child = null;
+                if (RED.settings.verbose) { node.log(RED._("bluetoothbutton.status.closed")); }
+                if (node.done) {
+                    node.status({fill:"grey",shape:"ring",text:"bluetoothbutton.status.closed"});
+                    node.done();
+                }
+                else { node.status({fill:"red",shape:"ring",text:"bluetoothbutton.status.stopped"}); }
+            });
 
-        node.child.on('error', function (err) {
-            if (err.errno === "ENOENT") { node.error(RED._("bluetoothbutton.errors.commandnotfound")); }
-            else if (err.errno === "EACCES") { node.error(RED._("bluetoothbutton.errors.commandnotexecutable")); }
-            else { node.error(RED._("bluetoothbutton.errors.error")+': ' + err.errno); }
+            node.child.on('error', function (err) {
+                if (err.errno === "ENOENT") { node.error(RED._("bluetoothbutton.errors.commandnotfound")); }
+                else if (err.errno === "EACCES") { node.error(RED._("bluetoothbutton.errors.commandnotexecutable")); }
+                else { node.error(RED._("bluetoothbutton.errors.error")+': ' + err.errno); }
+            });
         });
-        });
-
 
         node.on("close", function(done) {
             node.status({});
